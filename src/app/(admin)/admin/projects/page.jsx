2448d5325efae492fd6e3aa081218c6bb1019782
mykel68 <olowookeremicheal9568@gmail.com>
2024-05-21 11:00:29 +0100

    prod

--- src/app/(admin)/admin/projects/page.jsx
+++ src/app/(admin)/admin/projects/page.jsx
@@ -17,7 +17,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
   return (
